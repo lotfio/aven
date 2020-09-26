0b2f564fd7b3c10c22b0e9ff9fda984376521b32
lotfio <contact@lotfio.net>
2020-09-26 14:46:32 +0100

    test parse parser

---
+++ tests/Unit/RoutesParserTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Aven\RoutesParser;
+
+class RoutesParserTest extends TestCase
+{
+    /**
+     * example route
+     *
+     * @var array
+     */
+    protected $routes = [
+        [
+            'method'    => 'GET',
+            'uri'       => '',
+            'action'    => '',
+            'regex'     => '',
+            'name'      => '',
+            'group'     => '',
+            'namespace' => ''
+        ]
+    ];
+
+    /**
+     * test parse route uri
+     *
+     * @return void
+     */
+    public function testParseUri()
+    {
+        $parser = new RoutesParser;
+
+        $this->routes[0]['uri'] = 'test';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test$~', $this->routes[0]['uri']);
+
+        $this->routes[0]['uri'] = '/test/';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test$~', $this->routes[0]['uri']);
+    }
+
+    /**
+     * test parse route action
+     *
+     * @return void
+     */
+    public function testParseAction()
+    {
+        $parser = new RoutesParser;
+
+        $this->routes[0]['namespace'] = 'Test\\';
+        $this->routes[0]['action'] = 'TestClass';
+
+        $parser->parse($this->routes);
+        $this->assertSame('Test\TestClass', $this->routes[0]['action']);
+    }
+    
+    /**
+     * test parse predefined
+     *
+     * @return void
+     */
+    public function testParsePredefined()
+    {
+        $parser = new RoutesParser;
+
+        $this->routes[0]['uri'] = '/test/(:int)';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test/(\d+)$~', $this->routes[0]['uri']);
+
+        $this->routes[0]['uri'] = '/test/(:str)';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test/(\w+)$~', $this->routes[0]['uri']);
+
+        $this->routes[0]['uri'] = '/test/(:alpha)';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test/([A-z]+)$~', $this->routes[0]['uri']);
+
+        $this->routes[0]['uri'] = '/test/(:id*)';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test/?(\d*)$~', $this->routes[0]['uri']);
+
+        $this->routes[0]['uri'] = '/test/(:id?)';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test/(\d?)$~', $this->routes[0]['uri']);
+    }
+
+    /**
+     * test parse optional
+     *
+     * @return void
+     */
+    public function testParseOptional()
+    {
+        $parser = new RoutesParser;
+        $this->routes[0]['uri'] = '/test/(:id*)';
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test/?(\d*)$~', $this->routes[0]['uri']);
+    }
+
+    /**
+     * test parse user defined regex
+     *
+     * @return void
+     */
+    public function testParseRegex()
+    {
+        $parser = new RoutesParser;
+
+        $this->routes[0]['uri'] = '/test/(:id)';
+        $this->routes[0]['regex'] = ["(:id)" => "[custom-pattern]"];
+
+        $parser->parse($this->routes);
+        $this->assertSame('~^/test/([custom-pattern])$~', $this->routes[0]['uri']);
+    }
+}
